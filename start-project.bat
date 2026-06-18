3ced2bd6b1c2feccb0543185f33e45ce3746ee39
AdhamElHarouni-1 <a.tamer0312@gmail.com>
2026-06-18 20:03:57 +0300

    Restart stale server before launch

--- start-project.bat
+++ start-project.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :3000 ^| findstr LISTENING') do taskkill /PID %%a /F >nul 2>nul
 node scripts\seed.js
 node backend\server.js
